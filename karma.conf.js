822e50c7e0a0cafc3f7151b336fb17bc8e6cde26
Robert_Faskhutdinov <robfap@gmail.com>
2016-11-30 11:50:58 +0100

    add real test example

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
 
     files: [
+      './node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
       'app/scripts/**/*.*',
       'tests/**/*.js'
     ],
--- tests/component1/reducers/Comp1ReducerTest.js
+++ tests/component1/reducers/Comp1ReducerTest.js
@@ -1,7 +1,34 @@
+import comp1Reducer from 'component1/reducers/reducer';
+import * as actionTypes from 'component1/constants/actionTypes';
+
+var store;
+var defaultStore = {
+    counter: 0
+};
+
 describe('Comp1 reducer', () => {
 
     it('should check something', () => {
        expect(true).toEqual(true);
+    });
+
+    it ('should return default store', () => {
+        // When
+        store = comp1Reducer(store, {});
+
+        // Then
+        expect(store).toEqual(defaultStore);
+    });
+
+    it ('should increase counter', () => {
+       const action = {
+           type: actionTypes.TYPE_1
+       };
+
+       const newStore = comp1Reducer(store, action);
+
+        expect(newStore.counter).toEqual(1);
+
     });
 
 });
